9468f41891d64043fa0f009599b501537ce47b93
Kashyap Merai <kashyapk62@gmail.com>
2017-06-20 23:06:20 +0530

    "Enhancement Dynamic reload not workking"

--- pm2/app/app.js
+++ pm2/app/app.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 function onRequest(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
+  response.write("Hello World!!!");
   response.end();
 }
 
